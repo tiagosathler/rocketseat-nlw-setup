b156b350b19a6bcbaa4886d1876d86ec9bba1afd
Tiago Heringer Siqueira Sathler <sathler@gmail.com>
2023-01-19 17:38:36 -0300

    :sparkles: feat: finish web third day

--- web/src/components/SummaryTable.tsx
+++ web/src/components/SummaryTable.tsx
@@ -1,25 +1,27 @@
+import { MIN_SUMMARY_DATES_SIZE, WEEK_DAYS_LABELS } from '../utils/constants';
 import generateDatesFromYearBeginning from '../utils/generateRangeFromYearBeginning';
 import HabitDay from './HabitDay';
 import HabitDayEmpty from './HabitDayEmpty';
 import WeekDayLabel from './WeekDayLabel';
 
 export default function SummaryTable() {
-  const weekDays = ['D', 'S', 'T', 'Q', 'Q', 'S', 'S'];
   const summaryDays = generateDatesFromYearBeginning();
 
-  const MIN_SUMMARY_DATES_SIZE = 18 * 7;
   const amountOfDaysToFill = MIN_SUMMARY_DATES_SIZE - summaryDays.length;
+
+  const amount = summaryDays.length;
 
   return (
     <div className="w-full flex">
       <div className="grid grid-rows-7 grid-flow-row gap-3">
-        {weekDays.map((weekDay, i) => (
+        {WEEK_DAYS_LABELS.map((weekDay, i) => (
           <WeekDayLabel key={`week_day_label_${i}`} label={weekDay} />
         ))}
       </div>
+
       <div className="grid grid-rows-7 grid-flow-col gap-3">
-        {summaryDays.map((date, i) => (
-          <HabitDay key={`habit_day_${i}`} completed={i} />
+        {summaryDays.map((_date, i) => (
+          <HabitDay key={`habit_day_${i}`} amount={amount} completed={i} />
         ))}
 
         {amountOfDaysToFill > 0 &&
---
+++ web/src/utils/constants.ts
@@ -0,0 +1,3 @@
+export const MIN_SUMMARY_DATES_SIZE = 18 * 7; // 18 WEEKS
+
+export const WEEK_DAYS_LABELS = ['D', 'S', 'T', 'Q', 'Q', 'S', 'S'];
