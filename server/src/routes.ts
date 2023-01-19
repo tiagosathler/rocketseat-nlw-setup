9871c7d9d4662d10475733781b2fcfdc1793fafc
Tiago Heringer Siqueira Sathler <sathler@gmail.com>
2023-01-19 10:00:03 -0300

    :sparkles: feat: finish server third day

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -1,8 +1,16 @@
 import { FastifyInstance } from 'fastify';
-import { createHabitController, getDayController } from './controllers';
+import {
+  createHabitController,
+  getDayController,
+  toggleHabitController,
+  getSummaryController } from './controllers';
 
 export default async function appRoutes(app: FastifyInstance): Promise<void> {
   app.post('/habits', createHabitController);
 
   app.get('/day', getDayController);
+
+  app.patch('/habit/:id/toggle', toggleHabitController);
+
+  app.get('/summary', getSummaryController);
 }
