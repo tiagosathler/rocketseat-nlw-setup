9707f97659f7a1bef7e411347b1afec32e750173
Tiago Heringer Siqueira Sathler <sathler@gmail.com>
2023-01-20 20:41:53 -0300

    :bug: fix: change host address on server backend

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,7 +1,11 @@
+/* eslint-disable @typescript-eslint/restrict-template-expressions */
 /* eslint-disable @typescript-eslint/no-floating-promises */
 import Fastify from 'fastify';
 import cors from '@fastify/cors';
+import ip from 'ip';
 import appRoutes from './routes';
+
+const host = ip.address();
 
 const app = Fastify();
 
@@ -11,9 +15,10 @@ app.register(appRoutes);
 app
   .listen({
     port: 3333,
+    host,
   })
   .then(() => {
-    console.log('HTTP Server running on port 3333');
+    console.log(`HTTP Server listing on port http://${host}:3333`);
   })
   .catch(() => {
     console.error('Couldn\'t listen');
