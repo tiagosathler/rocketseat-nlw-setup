08f677f118c6f45fd9d0656d94e3cfab66efe3a4
Tiago Heringer Siqueira Sathler <sathler@gmail.com>
2023-01-20 20:43:02 -0300

    :sparkles: feat: finish mobile fourth day

--- mobile/src/components/HabitDay.tsx
+++ mobile/src/components/HabitDay.tsx
@@ -1,15 +1,34 @@
+import clsx from 'clsx';
+import dayjs from 'dayjs';
 import React from 'react';
 
 import { TouchableOpacity, TouchableOpacityProps } from 'react-native';
+import calculateProgressPercentage from '../utils/calculateProgressPercentage';
 
 import { DAY_SIZE } from '../utils/constants';
 
-type Props = TouchableOpacityProps;
+interface Props extends TouchableOpacityProps {
+  date: Date;
+  amount?: number;
+  completed?: number;
+}
 
-export default function HabitDay({ ...rest }: Props) {
+export default function HabitDay({ date, amount = 0, completed = 0, ...rest }: Props) {
+  const progress = calculateProgressPercentage(amount, completed);
+  const today = dayjs().startOf('day').toDate();
+  const isCurrentDay = dayjs(date).isSame(today);
+
   return (
     <TouchableOpacity
-      className="bg-zinc-900 rounded-lg border-2 m-1 border-zinc-800"
+      className={clsx('rounded-lg border-2 m-1', {
+        ['bg-zinc-900 border-zinc-800']: progress === 0,
+        ['bg-violet-900 border-violet-800']: progress > 0 && progress < 20,
+        ['bg-violet-800 border-violet-700']: progress >= 20 && progress < 40,
+        ['bg-violet-700 border-violet-600']: progress >= 40 && progress < 60,
+        ['bg-violet-600 border-violet-500']: progress >= 60 && progress < 80,
+        ['bg-violet-500 border-violet-400']: progress >= 80,
+        ['border-white border-4']: isCurrentDay,
+      })}
       style={{ width: DAY_SIZE, height: DAY_SIZE }}
       activeOpacity={0.7}
       {...rest}
--- mobile/src/screens/New.tsx
+++ mobile/src/screens/New.tsx
@@ -1,16 +1,49 @@
 /* eslint-disable react-native/no-inline-styles */
-import React, { useState } from 'react';
-import { ScrollView, View, Text, TextInput, TouchableOpacity } from 'react-native';
+import React, { useEffect, useState } from 'react';
+import { useNavigation } from '@react-navigation/native';
+
+import { ScrollView, View, Text, TextInput, TouchableOpacity, Alert } from 'react-native';
 import { Feather } from '@expo/vector-icons';
 import colors from 'tailwindcss/colors';
+
+import clsx from 'clsx';
 
 import BackButton from '../components/BackButton';
 import Checkbox from '../components/Checkbox';
 
+import api from '../lib/axios';
+
 import { WEEK_DAYS_NAMES } from '../utils/constants';
 
 export default function New() {
+  const { goBack } = useNavigation();
+
   const [weekDays, setWeekDays] = useState<number[]>([]);
+
+  const [title, setTitle] = useState<string>('');
+
+  const [enabledCheckbox, setEnabledCheckbox] = useState<boolean>(false);
+
+  function handleSubmit() {
+    api
+      .post('/habits', {
+        title: title.trim(),
+        weekDays,
+      })
+      .then(() => {
+        Alert.alert('Hábito criado com sucesso!');
+      })
+      .catch((e) => {
+        console.error(e);
+        Alert.alert('Falha na conexão com o servidor');
+      })
+      .finally(() => goBack());
+  }
+
+  useEffect(
+    () => setEnabledCheckbox(title.trim().length > 0 && weekDays.length > 0),
+    [title, weekDays],
+  );
 
   function handleToggleWeekDay(weekDayIndex: number) {
     if (weekDays.includes(weekDayIndex)) {
@@ -32,9 +65,11 @@ export default function New() {
         <Text className="mt-6 text-white font-semibold text-base">Qual seu comprometimento?</Text>
 
         <TextInput
-          className="h-12 pl-4 rounded-lg mt-3 bg-zinc-900 text-white border-2 border-zinc-800 focus:border-green-600"
+          className="h-12 pl-4 rounded-lg mt-3 bg-zinc-900 text-white border-2 border-zinc-800 focus:border-x-green-600"
           placeholder="Exercícios, dormir bem, etc"
           placeholderTextColor={colors.zinc[400]}
+          value={title}
+          onChangeText={setTitle}
         />
 
         <Text className="font-semibold mt-4 mb-3 text-white text-base">Qual a recorrência?</Text>
@@ -49,12 +84,29 @@ export default function New() {
         ))}
 
         <TouchableOpacity
-          className="w-full h-14 flex-row items-center justify-center bg-green-600 rounded-md mt-6"
+          className={clsx(
+            'w-full h-14 flex-row items-center justify-center rounded-md mt-6 bg-green-900',
+            {
+              ['bg-green-600']: enabledCheckbox,
+            },
+          )}
           activeOpacity={0.7}
+          onPress={() => handleSubmit()}
+          disabled={!enabledCheckbox}
         >
-          <Feather name="check" size={20} color={colors.white} />
+          <Feather
+            name="check"
+            size={20}
+            color={enabledCheckbox ? colors.white : colors.gray[500]}
+          />
 
-          <Text className="font-semibold text-base text-white ml-2">Confirmar</Text>
+          <Text
+            className={clsx('font-semibold text-base ml-2 text-gray-500', {
+              ['text-white']: enabledCheckbox,
+            })}
+          >
+            Confirmar
+          </Text>
         </TouchableOpacity>
       </ScrollView>
     </View>
